e1b4f9ba23fe39a67231d4af166904031ccaa701
Paul Layacan <paul.layacan@yahoo.com>
2021-08-03 21:32:22 +0800

    Add Loader and Message Component Template

---
+++ client/src/components/loader/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div style={{width : '50px', height: '50px'}} className="spinner-border"></div>
+    )
+}
+
+export default Loader
